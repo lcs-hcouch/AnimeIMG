4d4f586feed8a48d7a3da7100ba215dccfb18806
Hayden Couch <hcouch@lcs.on.ca>
2021-10-06 10:51:39 -0400

    Initial Commit

---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Hayden Couch on 2021-10-06.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
